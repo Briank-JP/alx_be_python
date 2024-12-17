32e3089a1ad0728055dbfea8faaea858e51bc724
Kaheru <kaherubrian2016@gmail.com>
2024-12-17 22:05:33 +0300

    basic py operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10 
+number2 = 5
+# addition 
+sum1 = number1 + number2
+print(f"addition of {number1} and {number2} is {sum1}")
+
+# subtraction
+diff = number1 - number2
+print("subtraction of " +   str(number1) + " and " + str(number2) + " is " + str(diff))
+
+# multiplication
+
+product = number1 * number2
+print(f"multiplication of {number1} and {number2} is {product}")
