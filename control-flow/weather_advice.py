1279a2e2d0bc5bbeed79e1fd11457ae2b05b374c
Kaheru <kaherubrian2016@gmail.com>
2024-12-19 16:32:45 +0300

    weather_advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
 # create a variable to store the user input
 weather = input("what's the weather like today? (sunny/rainy/cold):")
 if weather == "sunny":
-    print("Wear a t-shirt and sunglasses")
-elif weather == "rainny":
-    print("Dont forget your umbrella and a raincoat")
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf")
 else:
