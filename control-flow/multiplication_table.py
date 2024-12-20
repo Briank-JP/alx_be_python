38e1daf098ef26b5f415bab45e5d49ab18ca2773
Kaheru <kaherubrian2016@gmail.com>
2024-12-20 16:00:37 +0300

    multipication table.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,9 +3,12 @@
 num1 = int(input("Enter a number to see its multiplication table: "))
 
 # print the multiplication table for the given number
-print(f'multiplication table for {num1} is: ')
+# print(f'multiplication table for {num1} is: ')
 
 # loop through the range from 1 to 10 (inclusive) and calculate the product for each number
 for i in range(1,11):
+    # culculate the product of the number enterd and the range 
     product = num1 * i
+    # print the result.
     print(f'{num1} * {i} = {product}')
+    print()
