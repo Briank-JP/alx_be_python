9c20f883fa64807c3aa0a653e8a6d91f97c98f54
kaheru brian <kaherubrian2016@gmail.com>
2025-01-08 00:46:56 +0300

    function operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
    if operation == "add":
       return num1 + num2
    elif operation == "subtract":
-      return num2 - num1
+      return num1 - num2
    elif operation == "divide":
       if num2 == 0:
          return "error, number not divisible by 0"
