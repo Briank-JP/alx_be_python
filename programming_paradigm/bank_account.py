e18b3225f013aefe5500ce98b09ac3cd4fba5a26
Kaheru <kaherubrian2016@gmail.com>
2025-01-18 00:38:01 +0300

    added a bank account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return "Insufficient funds"
+        self.account_balance -= amount
+        return self.account_balance
+    
+    def bank_balance(self):
+       print(f"Current Balance: ${self.account_balance}")
