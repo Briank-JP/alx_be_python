788663c552bd5cf5556e7694f967d8da767d9c77
Kaheru <kaherubrian2016@gmail.com>
2025-01-18 00:46:28 +0300

    added the main file and bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,10 @@ class BankAccount:
         return self.account_balance
     
     def withdraw(self, amount):
-        if amount > self.account_balance:
-            return "Insufficient funds"
-        self.account_balance -= amount
-        return self.account_balance
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
     
     def display_balance(self):
        print(f"Current Balance: ${self.account_balance}")
