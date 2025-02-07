4c88dab8b400806aef1434e415deabde220699f3
Kaheru <kaherubrian2016@gmail.com>
2025-02-07 15:08:50 +0300

    added simple calculator test cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,17 +10,17 @@ class TestCalcNumbers(unittest.TestCase):
         self.assertEqual(self.calc.add(2,5), 7)
         self.assertEqual(self.calc.add(4,8), 12)
         
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3), 2)
         self.assertEqual(self.calc.subtract(7,5), 2)
         self.assertEqual(self.calc.subtract(10,8), 2)
         
-    def test_multipy(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3,5), 15)
         self.assertEqual(self.calc.multiply(2,7), 14)
         self.assertEqual(self.calc.multiply(4,8), 32)
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,2), 5)
         self.assertEqual(self.calc.divide(14,7), 2)
         self.assertEqual(self.calc.divide(16,2), 8)
